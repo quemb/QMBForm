dfcd2d8210aa4f07073888703757dbd5c32b3204
Patrick McKeown <pmaccamp@sbcglobal.net>
2015-09-24 17:47:34 -0700

    Clarify descriptions further

--- lib/QMBForm/src/main/java/com/quemb/qmbform/descriptor/CellConfigObject.java
+++ lib/QMBForm/src/main/java/com/quemb/qmbform/descriptor/CellConfigObject.java
@@ -6,16 +6,16 @@ package com.quemb.qmbform.descriptor;
 public class CellConfigObject {
     public enum CONFIG_TYPE {
         PADDING, // Specified as an int[4] in the order {left, top, right, bottom}
-        TYPEFACE,  // Specified as a Typeface enum such as Typeface.BOLD
-        INPUT_TYPE,  // Specified as an InputType enum such as InputType.TYPE_NUMBER_FLAG_DECIMAL
+        TYPEFACE,  // Specified as a Typeface enum such as Typeface.BOLD.  Only applicable to instances of TextView
+        INPUT_TYPE,  // Specified as an InputType enum such as InputType.TYPE_NUMBER_FLAG_DECIMAL.  Only applicable to instances of TextView
         TEXT_ALIGNMENT, // Specified as an View enum such as TEXT_ALIGNMENT_CENTER (only available on api 17+)
-        MAX_LINES, // Specified as an int
-        MIN_LINES, // Specified as an int
+        MAX_LINES, // Specified as an int.  Only applicable to instances of TextView
+        MIN_LINES, // Specified as an int.  Only applicable to instances of TextView
         MINIMUM_HEIGHT, // Specified as an int
         MINIMUM_WIDTH, // Specified as an int
         BACKGROUND_COLOR, // Specified as color reference id
-        TEXT_COLOR, // Specified as color reference id
-        GRAVITY // Specified as a Gravity enum such as Gravity.CENTER
+        TEXT_COLOR, // Specified as color reference id.  Only applicable to instances of TextView
+        GRAVITY // Specified as a Gravity enum such as Gravity.CENTER.  Only applicable to instances of TextView
     }
 
     public CONFIG_TYPE configType;
