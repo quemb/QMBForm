6e5a3700d1da13e00e78a72a6a2a7927a36f699b
philippe_tran <tran.dtp@gmail.com>
2016-07-07 16:36:23 +0200

    Update CellDescriptor.java

--- lib/QMBForm/src/main/java/com/quemb/qmbform/descriptor/CellDescriptor.java
+++ lib/QMBForm/src/main/java/com/quemb/qmbform/descriptor/CellDescriptor.java
@@ -8,10 +8,10 @@ public class CellDescriptor
 {
     // TextAppearance style ID
 
+    public static final String APPEARANCE_SECTION    = "FORM_APPEARANCE_SECTION";     // value is an attribute ID as Integer.valueOf(R.attr.xxx)
     public static final String APPEARANCE_TEXT_LABEL = "FORM_APPEARANCE_TEXT_LABEL";  // value is an attribute ID as Integer.valueOf(R.attr.xxx)
     public static final String APPEARANCE_TEXT_VALUE = "FORM_APPEARANCE_TEXT_VALUE";  // value is an attribute ID as Integer.valueOf(R.attr.xxx)
-
-    public static final String APPEARANCE_BUTTON = "FORM_APPEARANCE_BUTTON";          // value is an attribute ID as Integer.valueOf(R.attr.xxx)
+    public static final String APPEARANCE_BUTTON     = "FORM_APPEARANCE_BUTTON";      // value is an attribute ID as Integer.valueOf(R.attr.xxx)
 
     // Text colors (if TextAppearance styles are not defined)
 
