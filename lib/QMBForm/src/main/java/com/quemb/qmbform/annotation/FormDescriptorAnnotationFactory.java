9ba1012a7c5040490852ff2e9a2d2d1e3e530cec
philippe_tran <tran.dtp@gmail.com>
2016-07-08 11:59:47 +0200

    Update FormDescriptorAnnotationFactory.java

--- lib/QMBForm/src/main/java/com/quemb/qmbform/annotation/FormDescriptorAnnotationFactory.java
+++ lib/QMBForm/src/main/java/com/quemb/qmbform/annotation/FormDescriptorAnnotationFactory.java
@@ -14,6 +14,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 /**
