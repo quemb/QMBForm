5a7b92396797e19a895c64e40bb7a2c155b72226
philippe_tran <tran.dtp@gmail.com>
2016-07-18 14:14:54 +0200

    Update FormDetailTextInlineFieldCell.java

--- lib/QMBForm/src/main/java/com/quemb/qmbform/view/FormDetailTextInlineFieldCell.java
+++ lib/QMBForm/src/main/java/com/quemb/qmbform/view/FormDetailTextInlineFieldCell.java
@@ -24,8 +24,23 @@ public class FormDetailTextInlineFieldCell extends FormTitleFieldCell {
 
         super.init();
         mDetailTextView = (TextView) findViewById(R.id.detailTextView);
+        if (setStyleId(mDetailTextView, CellDescriptor.APPEARANCE_TEXT_VALUE, CellDescriptor.COLOR_VALUE) == false)
+        {
+            // If no specific style is defined for APPEARANCE_TEXT_VALUE,
+            // set inline text size to the default EditText size.
 
-        setStyleId(mDetailTextView, CellDescriptor.APPEARANCE_TEXT_VALUE, CellDescriptor.COLOR_VALUE);
+            // Get the android:textAppearance item from R.style.Widget_AppCompat_EditText (default EditText style)
+            int editTextAppearanceId = getStyleItemResourceId(mDetailTextView.getContext(),
+                    R.style.Widget_AppCompat_EditText, android.R.attr.textAppearance, android.R.attr.textAppearanceMediumInverse);
+
+            // Get the android:textSize item from retrieved textAppearance style
+            DisplayMetrics displayMetrics = mDetailTextView.getContext().getResources().getDisplayMetrics();
+            float editSize = getStyleItemDimension(mDetailTextView.getContext(),
+                    editTextAppearanceId, android.R.attr.textSize, 18f * (displayMetrics.densityDpi / 160f));
+
+            // Set inline text size
+            mDetailTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, editSize);
+        }
     }
 
     @Override
@@ -55,5 +70,48 @@ public class FormDetailTextInlineFieldCell extends FormTitleFieldCell {
 
     public TextView getDetailTextView() {
         return mDetailTextView;
+    }
+
+    /**
+     * Get a item defined as resource ID from an android style
+     */
+    private int getStyleItemResourceId(final Context context, final int styleId, final int attributeId, final int defValue)
+    {
+        // attribute to retrieve
+        int[] attrs = {attributeId}; // {android.R.attr.textColor, android.R.attr.text};
+
+        // Parse the style, using Context.obtainStyledAttributes()
+        TypedArray ta = context.obtainStyledAttributes(styleId, attrs);
+
+        // Fetch the resource, color or text defined in your style
+        int styleItem = ta.getResourceId(0, defValue); // android.R.attr.textAppearanceMediumInverse);
+        //int textColor = ta.getColor(1, Color.BLACK);
+        //String text = ta.getString(2);
+
+        // Recycle the TypedArray
+        ta.recycle();
+
+        return styleItem;
+    }
+
+
+    /**
+     * Get a item defined as dimension from an android style
+     */
+    private float getStyleItemDimension(final Context context, final int styleId, final int attributeId, final float defValue)
+    {
+        // attribute to retrieve
+        int[] attrs = {attributeId};
+
+        // Parse the style, using Context.obtainStyledAttributes()
+        TypedArray ta = context.obtainStyledAttributes(styleId, attrs);
+
+        // Fetch the text from your style like this.
+        float dimensionItem = ta.getDimension(0, defValue);
+
+        // Recycle the TypedArray
+        ta.recycle();
+
+        return dimensionItem;
     }
 }
