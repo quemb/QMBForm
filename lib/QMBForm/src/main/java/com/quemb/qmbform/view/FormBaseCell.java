1c9e6a2f4377e568cc397d2b74ddece3cc091ee8
Philippe <tran.dtp@gmail.com>
2016-07-28 14:07:03 +0200

    Vector drawables for gradle 1.5

--- lib/QMBForm/src/main/java/com/quemb/qmbform/view/FormBaseCell.java
+++ lib/QMBForm/src/main/java/com/quemb/qmbform/view/FormBaseCell.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatDrawableManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
